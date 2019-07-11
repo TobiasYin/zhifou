6d2aabf7110cfca34bcc2116a1651cf81954ef24
TobiasYin <yrt7296@gmail.com>
2019-07-11 09:51:38 +0800

    Basicly finish

--- db/test_data.sql
+++ db/test_data.sql
@@ -131,7 +131,7 @@ from (select id, question_id, user_id, content, time
       where user_id in (select followed_id from follow where follow_id = 'bb105db1addc44ad83a60de84b0863cd')) as A
          natural join (select count(*) as agrees, answer_id as id from agree group by answer_id) as f
 order by agrees - (now() - time) / 1800 desc
-limit 20
+limit 20;
 
 select *
 from follow
--- src/beans/Answer.java
+++ src/beans/Answer.java
@@ -5,7 +5,6 @@ import util.GetUUID;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
